ff64931df8a2476ea3414fa8dad8f8cdfe59684e
Sheikh Eismile Ahmed Sayem <ahmedsayem730@gmail.com>
2024-09-18 23:30:23 +0600

    lab-03 task-04 ADD

--- lab-03/task-04.asm
+++ lab-03/task-04.asm
@@ -7,7 +7,8 @@ NEWLINE db 0Dh, 0Ah, '$'
 M1 db "Enter a number: $"
 M2 db "Enter another Number: $"
 M3 db "The sum of $"
-M4 db " is "
+M4 db " and $"
+M5 db " is $"
 
 .CODE
 MAIN PROC
@@ -15,6 +16,10 @@ MAIN PROC
 MOV AX,@DATA
 MOV DS,AX
 ;enter your code here
+
+    ;Write a program to:
+        ;a. Read two decimal digits whose sum is less than 10
+        ;b. Display them and their sum on the next line, with an appropriate message.
     
     LEA DX, M1
     MOV AH, 9
@@ -23,8 +28,12 @@ MOV DS,AX
     MOV AH, 1
     INT 21h
     
-    MOV AL, 30h
+    SUB AL, 30h
     MOV BL, AL
+    
+    LEA DX, NEWLINE
+    MOV AH, 9
+    INT 21h 
     
     LEA DX, M2
     MOV AH, 9
@@ -33,9 +42,42 @@ MOV DS,AX
     MOV AH, 1
     INT 21h
     
-    MOV AL, 30h
+    SUB AL, 30h
     MOV BH, AL
+    MOV CL, AL
     
+    ADD CL,BL
+    
+    LEA DX, NEWLINE
+    MOV AH, 9
+    INT 21h
+             
+    LEA DX, M3
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 2
+    MOV DL, BL
+    ADD DL, 30h
+    INT 21h  
+    
+    LEA DX, M4
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 2
+    MOV DL, BH
+    ADD DL, 30h
+    INT 21h
+    
+    LEA DX, M5
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 2
+    MOV DL, CL
+    ADD DL, 30h
+    INT 21h
     
 
 
