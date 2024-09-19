54bb03713c52c8ecffaf1a4c61adfcd57005c994
Sheikh Eismile Ahmed Sayem <ahmedsayem730@gmail.com>
2024-09-19 11:29:51 +0600

    lab-03 task-1 and task-04 ADD

--- lab-03/task-01.asm
+++ lab-03/task-01.asm
@@ -1,6 +1,10 @@
-                  .MODEL SMALL
+.MODEL SMALL
 .STACK 100H
 .DATA
+
+M1 db "Please insert a character: $"
+NEWLINE db 0Dh, 0Ah, '$'
+
 .CODE
 MAIN PROC
 ;iniitialize DS
@@ -8,18 +12,24 @@ MOV AX,@DATA
 MOV DS,AX
 ;enter your code here
 ;Take a character input and display it. Display the message "Please insert a character: " when taking an input.
-
-
-
-
-
-
-
-
-
-
-
-
+    
+    LEA DX, M1
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 1
+    INT 21h
+    
+    ADD AL, 20h
+    MOV BL, AL
+    
+    LEA DX, NEWLINE
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 2
+    MOV DL, BL
+    INT 21h
 
 
 ;exit to DOS
--- lab-03/task-02.asm
+++ lab-03/task-02.asm
@@ -1,15 +1,112 @@
-                  .MODEL SMALL
+.MODEL SMALL
 .STACK 100H
 .DATA
+
+NEWLINE db 0Dh, 0Ah, '$'
+
+M1 db "Enter a number: $"
+M2 db "Enter another Number: $"
+
+A db "Addition: $"
+S db "Subtraction: $"
+M db "Multiplication: $"
+D db "Division: $"
+
+
 .CODE
 MAIN PROC
 ;iniitialize DS
 MOV AX,@DATA
 MOV DS,AX
-;enter your code here
-;Perform addition/subtraction/division/multiplication by taking inputs from the user. Note: Display appropriate messages when taking input and showing the output.
+    ;enter your code here
+    ;Perform addition/subtraction/division/multiplication by taking inputs from the user. Note: Display appropriate messages when taking input and showing the output.
 
-
+    LEA DX, M1
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 1
+    INT 21h
+    
+    SUB AL, 30h
+    MOV BL, AL
+    
+    LEA DX, NEWLINE
+    MOV AH, 9
+    INT 21h
+    
+    LEA DX, M2
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 1
+    INT 21h
+    
+    ;ADDITION
+    
+    SUB AL, 30h
+    MOV BH, AL
+    
+    MOV CL, BL
+    MOV AL, BL
+    
+    MOV CH, BH
+    MOV AH, BH
+    
+    ADD AL, CH
+    ADD AL, 30h
+    MOV CL, AL
+    
+    LEA DX, NEWLINE
+    MOV AH, 9
+    INT 21h
+    
+    LEA DX, A
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 2
+    MOV DL, CL
+    INT 21h
+    
+    ;SUBTRACTION
+    
+    MOV CL, BL
+    MOV AL, BL
+    
+    MOV CH, BH
+    MOV AH, BH
+    
+    SUB AL, CH
+    ADD AL, 30h
+    MOV CL, AL
+    
+    LEA DX, NEWLINE
+    MOV AH, 9
+    INT 21h
+    
+    LEA DX, S
+    MOV AH, 9
+    INT 21h
+    
+    MOV AH, 2
+    MOV DL, CL
+    INT 21h
+    
+    LEA DX, NEWLINE
+    MOV AH, 9
+    INT 21h
+    
+    ;MULTIPLICATION
+    
+    
+    
+    ;DIVISION
+    
+    
+    
+    
+        
 
 
 
